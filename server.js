fc92c8e746f24825215f9ca09dcc66f5822ad9b0
Robin <accounts@ganttify.com>
2021-08-19 11:03:15 -0400

    basic socket.io server
    Signed-off-by: Robin <accounts@ganttify.com>

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-'use strict';
+const options = {
+    cors: ['http://localhost:*', 'http://kok.cattlea.com:*'],
+    serveClient: false
+};
 
-const express = require('express');
-const socketIO = require('socket.io');
+const io = require("socket.io")(options);
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
-const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+io.on("connection", socket => {
+    console.log('Received a new connection');
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+io.listen(3000);
