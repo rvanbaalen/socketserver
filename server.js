97695f2517870cbc5f47514cc8c926540367fbaf
Robin <accounts@ganttify.com>
2021-08-19 11:15:34 -0400

    added username middleware

--- server.js
+++ server.js
@@ -9,4 +9,15 @@ io.on("connection", socket => {
     console.log('Received a new connection');
 });
 
+// Username middleware
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username;
+    if (!username) {
+        return next(new Error("invalid username"));
+    }
+
+    socket.username = username;
+    next();
+});
+
 io.listen(process.env.PORT || 3000);
