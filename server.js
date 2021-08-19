a6edb5c834c64d439d622f54ed272731bb3a566a
Robin <accounts@ganttify.com>
2021-08-19 13:39:49 -0400

    implemented first lobby logic

--- server.js
+++ server.js
@@ -11,28 +11,29 @@ io.on("connection", socket => {
 
 // Username middleware
 io.use((socket, next) => {
-    const username = socket.handshake.auth.username;
+    const {username, lobby} = socket.handshake.auth;
     if (!username || username === '') {
         return next(new Error("invalid username"));
     }
 
     socket.username = username;
+    socket.lobby = lobby;
     next();
 });
 
 io.on("connection", (socket) => {
     const users = [];
     for (let [id, socket] of io.of("/").sockets) {
-        const {username} = socket;
-        users.push({id, username});
+        const {username, lobby} = socket;
+        users.push({id, username, lobby});
     }
     socket.emit("users", users);
 });
 
 io.on("connection", (socket) => {
     // notify existing users
-    const {id, username} = socket;
-    socket.broadcast.emit("user connected", {id, username});
+    const {id, username, lobby} = socket;
+    socket.to(lobby).emit("user connected", {id, username});
 });
 
 io.listen(process.env.PORT || 3000);
